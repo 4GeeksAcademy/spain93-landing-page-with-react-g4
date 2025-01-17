0e28891a30c56c4c02335411539de48d05a44e31
MauroG07 <71202727+MauroG07@users.noreply.github.com>
2025-01-17 19:45:18 +0000

    Footer

---
+++ src/js/component/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const Footer = () => {
+
+
+    return (
+        <div className="container">
+            <nav className="navbar bg-dark">
+                <div className="container-fluid justify-content-center">
+                    <span className="navbar-text text-white">
+                    Copyright ® Your Website 2019
+                    </span>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
